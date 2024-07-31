65167e4a0bf82a53b16c93d232eb78137ba41491
Michael <114317937+thatonecodes@users.noreply.github.com>
2024-07-31 00:20:38 -0400

    fix to urls.py, fix views.py general layouting

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,2 +1,6 @@
+from django.urls.conf import path, include
+
+
 urlpatterns = [
+        #create api endpoints here, path, include
 ]
--- src/views.py
+++ src/views.py
@@ -3,6 +3,11 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = "home.html"
+    title = "Home"
+    documentTitle = "" # root will be hackACI
 
 class AboutView(TemplateView):
     template_name = "about.html"
+    title = "About Us"
+    documentTitle = " - About"
+
