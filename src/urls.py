4b7d953a64735f35ac6e35397b392e178401e79e
Michael <114317937+thatonecodes@users.noreply.github.com>
2024-07-31 00:55:52 -0400

    add custom context for document title in views

--- src/urls.py
+++ src/urls.py
@@ -3,7 +3,7 @@ from src.api import urls as apiurls
 from src import views  
 
 urlpatterns = [
-    path("", views.IndexView.as_view(), name="home"),
+    path("", views.IndexView.as_view(), name=views.IndexView.name),
     path("api/", include(apiurls)),
-    path("aboutus/", views.AboutView.as_view(), name="About Us"),
+    path("aboutus/", views.AboutView.as_view(), name=views.AboutView.name),
 ]
--- src/views.py
+++ src/views.py
@@ -3,11 +3,20 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = "home.html"
-    title = "Home"
-    documentTitle = "" # root will be hackACI
+    name = "Home"
+    documentTitle = "hackACI" # root will be hackACI
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["documentTitle"] = self.documentTitle
+        return context
 
 class AboutView(TemplateView):
     template_name = "about.html"
-    title = "About Us"
-    documentTitle = " - About"
+    name = "About Us"
+    documentTitle = "hackACI - About"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["documentTitle"] = self.documentTitle
+        return context
